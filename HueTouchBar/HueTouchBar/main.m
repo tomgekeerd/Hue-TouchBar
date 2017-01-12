94f7f8a5177cf15ce3e51d3dbcafd8f1109cf5b9
tomgekeerd <tomderuiter.hhw@hotmail.com>
2017-01-12 22:11:04 +0100

    Initial commit

---
+++ HueTouchBar/HueTouchBar/main.m
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ Copyright (c) 2013 Koninklijke Philips N.V.
+ All Rights Reserved.
+ ********************************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
